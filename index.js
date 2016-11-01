b1f0d0fbd3320e8560b8762b94183128eae3b5cb
Student <jessenuese@gmail.com>
2016-11-01 10:37:39 -0600

    Basic functioning build

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+
+    socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+    });
+
+    socket.on('disconnect', function(){
+    console.log('user disconnected');
+    });
 });
+
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
