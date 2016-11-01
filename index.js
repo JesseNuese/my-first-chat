8f1c21d8e6eedb30d2b5d375b42a8b9014f51195
Student <jessenuese@gmail.com>
2016-11-01 11:25:30 -0600

    In 2 deep

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
+var io = require('socket.io');
+var socket = io.listen(server);
+var people = {};
+var fs = require('fs');
 
+fs.readFile('./index.html', function (err, data){
+  if (err) {
+    throw err;
+  }
+  index = data;
+});
+
+var server = http.createServer(function(request, response){
+  response.writeHeader(200,{'Content-Type': 'text/html'});
+  response.write(index);
+  response.end();
+}).listen(3000);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-
-    socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
+socket.on('connection', function(client){
+    client.on('join', function(name){
+      people[client.id] = name;
+      client.emit('update', 'You have connected to the server.');
+      socket.sockets.emit('update', name + 'has joined the server.')
+      socket.sockets.emit('update-people', people);
     });
-
-    socket.on('disconnect', function(){
-    console.log('user disconnected');
+    client.on('send', function(msg){
+      socket.sockets.emit('chat', people[client.id], msg);
+    });
+    client.on('disconnect', function(){
+      socket.sockets.emite('update', people[client.id]+'has left the server.');
+      delete people[client.id];
+      socket.sockets.emit('update-people', people);
     });
 });
-
 
 
 
